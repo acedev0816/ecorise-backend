c2fadc7f72f8c3c7aa02e4c1aafdc79547037056
acedev0816 <acedev0816@gmail.com>
2024-02-22 22:31:34 +0300

    work

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import bodyParser from 'body-parser';
+import morgan from "morgan";
+import path from "path";
 
 // import routes
-import utilityRoute from "./routes/utility.js"
+import nftRoute from "./routes/nft.js"
 
 // parse env file
 dotenv.config();
@@ -16,11 +18,15 @@ const port = process.env.PORT || 8000;
 // using dependencies
 app.use(cors());
 app.use(express.json());
+app.use(morgan());
 app.use(bodyParser.json({ limit: "5mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "5mb", extended: true }));
 
+// set assets folder
+app.use('/uploads', express.static('uploads'));
+
 // setup routers
-app.use("/utility", utilityRoute)
+app.use("/nft", nftRoute)
 app.get('/', (req, res) => {
   res.send('Welcome to Express & TypeScript Server');
 });
