eee14bfcdd8e1e05af5ecfceaab6cb7064434a3c
YakivMart <yakivmartynenko@gmail.com>
2024-02-22 04:28:26 +0200

    add mintnftwithID function

--- src/controllers/utility.js
+++ src/controllers/utility.js
@@ -49,3 +49,43 @@ export const mintnft = async (req, res)=>{
     .then(response => console.log(response))
     .catch(err => console.error(err));
 }
+
+export const mintnftwithID = async (req, res)=>{
+  const {media_file, title, description, category, tags, duration, chain} = req;
+  const apiKey        = process.env.API_KEY;
+  const is_compressed = true, is_reuploadLinkedFiles = true; // true, false available
+  const animation_url = '', trait_type = 'trait_type', attr_value = 'attr_test';
+  const display_type  = 'boost_number'; // boost_number, boost_percentage, number string available
+  const idempotencyKey = '';
+  const url           = `https://${process.env.PLATFORM}.crossmint.com/api/2022-06-09/collections/${process.env.COLLECTION_ID}/nfts/${idempotencyKey}`;
+  const recipient     = `email:${process.env.RECIPIENT_EMAIL}:${chain}`;
+  const options = {
+    method: 'POST',
+    headers: {'X-API-KEY': apiKey, 'Content-Type': 'application/json'},
+    body: JSON.stringify({
+      compressed: is_compressed,
+      metadata: {
+        animation_url: animation_url,
+        attributes: [
+          {
+            display_type: display_type,
+            trait_type: trait_type,
+            value: attr_value,
+            category: category,
+            tags: tags,
+            duration: duration
+          }
+        ],
+        description: description,
+        image: media_file,
+        name: title
+      },
+      recipient: recipient,
+      reuploadLinkedFiles: is_reuploadLinkedFiles
+    }),
+  };
+  fetch(url, options)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
+}
