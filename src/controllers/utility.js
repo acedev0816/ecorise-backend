7fca582a78bb8fe54c0b8bd913bdfc1ad31be572
YakivMart <yakivmartynenko@gmail.com>
2024-02-22 08:10:25 +0200

    add mintStatus function, delete duplicated mintStatus function, modify mintnft and  mintnftwithID functioin

--- src/controllers/utility.js
+++ src/controllers/utility.js
@@ -12,7 +12,7 @@ export const upload = async (req, res) => {
 // nft mint
 /////////////////////////
 export const mintnft = async (req, res)=>{
-  const {media_file, title, description, category, tags, duration, chain} = req;
+  const {media_file, title, description, category, tags, duration, chain} = req.body;
   const apiKey        = process.env.API_KEY;
   const is_compressed = true, is_reuploadLinkedFiles = true; // true, false available
   const animation_url = '', trait_type = 'trait_type', attr_value = 'attr_test';
@@ -41,7 +41,7 @@ export const mintnft = async (req, res)=>{
   };
   fetch(url, options)
     .then(response => response.json())
-    .then(response => {mintStatus(apiKey, process.env.PLATFORM, response.actionId);console.log(response)})
+    .then(response => {mintStatus(apiKey, process.env.PLATFORM, response.actionId)})
     .catch(err => console.error(err));
 }
 function mintStatus(apiKey, env, actionId){
@@ -57,7 +57,7 @@ function mintStatus(apiKey, env, actionId){
     .catch((err) => console.error(err));
 }
 export const mintnftwithID = async (req, res)=>{
-  const {media_file, title, description, category, tags, duration, chain} = req;
+  const {media_file, title, description, category, tags, duration, chain} = req.body;
   const apiKey        = process.env.API_KEY;
   const is_compressed = true, is_reuploadLinkedFiles = true; // true, false available
   const animation_url = '', trait_type = 'trait_type', attr_value = 'attr_test';
@@ -96,20 +96,32 @@ export const mintnftwithID = async (req, res)=>{
     .catch(err => console.error(err));
 }
 
-export const mintStatus = async (req, rest)=>{
-  const apiKey = process.env.API_KEY;
-  const chain = "polygon"; // or "solana", "ethereum", ...
-  const env = "staging"; // or "www"
-  const actionId = "<MINT_ACTION_ID>";
+export const editNFT = async (req, rest)=>{
+  const {media_file, title, description, action_id, is_reuploadLinkedFiles} = req.body;
+  const animation_url = '';
+  const display_type = 'boost_number', trait_type = 'trait_type', attr_value = 'attr_test';
 
-  const url = `https://${env}.crossmint.com/api/2022-06-09/actions/${actionId}`;
   const options = {
-    method: "GET",
-    headers: { "X-API-KEY": apiKey },
+    method: 'PATCH',
+    headers: {'X-API-KEY': process.env.API_KEY, 'Content-Type': 'application/json'},
+    body: JSON.stringify({
+      metadata: {
+        animation_url: animation_url,
+        attributes:[
+          {
+            display_type: display_type, trait_type: trait_type, value: attr_value, category: category, tags: tags, duration: duration
+          }
+        ],
+        description: description,
+        image: media_file,
+        name: title
+      },
+      reuploadLinkedFiles:is_reuploadLinkedFiles
+    }),
   };
-
-  fetch(url, options)
-    .then((response) => response.json())
-    .then((response) => console.log(response))
-    .catch((err) => console.error(err));
+  
+  fetch(`https://${process.env.PLATFORM}.crossmint.com/api/2022-06-09/collections/${process.env.COLLECTION_ID}/nfts/${action_id}`, options)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
 }
