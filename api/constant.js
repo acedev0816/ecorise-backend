5e2733a4dbe8591b0730648bf7a80dcddec8f7e4
acedev0816 <acedev0816@gmail.com>
2024-02-26 13:34:27 +0300

    change collection

--- api/constant.js
+++ api/constant.js
@@ -5,5 +5,6 @@ dotenv.config();
 export const API_KEY = process.env.API_KEY;
 export const NET_ENV = process.env.STAGING? 'staging' : 'www';
 
-export const POLYGON_COLLECTION_ID = "be12b4b4-9c6f-42c2-ad57-6df8fa67c173"
+export const POLYGON_COLLECTION_ID = "6103a9d9-d9b9-4509-9de7-77688a75a9a7"
+
 export const ENVIRONMENT = process.env.ENVIRONMENT || "develop"
